2efc4d2d3a0daacc866a321f2ec81eeecb59898f
İsmail Han <cilginsinek58@gmail.com>
2025-05-17 20:36:56 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import AuthPage from "@/components/AuthComponent";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -8,17 +7,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-gray-100">
-      <h1 className="text-4xl font-bold mb-4 text-blue-400">Çekiyo</h1>
-      {!showAuth ? (
-        <button
+      <h1 className="text-4xl font-bold mb-4 text-blue-400">Çekiyo</h1> 
+        <Link
+          href={"/auth"}
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-800 transition duration-300 text-center sm:px-6 sm:py-3 sm:text-lg"
-          onClick={() => setShowAuth(true)}
         >
           Giriş Yap
-        </button>
-      ) : (
-        <AuthPage />
-      )}
+        </Link>
     </div>
   );
 }
